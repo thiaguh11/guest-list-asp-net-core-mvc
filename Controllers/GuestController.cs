101d537bd74fb6e9dc433cb61b46d9b4aa9cedb8
thiaguh11 <thiagosilva110299@gmail.com>
2019-07-05 17:19:36 -0300

    Add Project in Git Repository

---
+++ Controllers/GuestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuestList.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestList.Controllers
+{
+    public class GuestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FormGuest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FormGuest(Guest g)
+        {
+            Repository.AddGuest(g);
+            return View("Thanks", g);
+        }
+
+        public IActionResult List()
+        {
+            return View(Repository.AllGuest().Where(guest => guest.Status == true));
+        }
+
+        public IActionResult Thanks()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuestList.Models
+{
+    public static class Repository
+    {
+        private static List<Guest> Guests = new List<Guest>();
+
+        public static void AddGuest(Guest g)
+        {
+            Guests.Add(g);
+        }
+
+        public static IEnumerable<Guest> AllGuest()
+        {
+            return Guests;
+        }
+    }
+}
